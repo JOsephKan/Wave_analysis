a2793dfbdcf6569fc84fb471465a3e063f62d388
JOsephKan <b11209013@ntu.edu.tw>
2024-05-06 11:44:43 +0800

    data_process.py update

--- python_package/data_process.py
+++ python_package/data_process.py
@@ -1,62 +1,88 @@
 # import pamonth_data
 import numpy as np
 
-# class for data processing
-class pro:
-    def __init__(self):
-        self.sym_lat = None
-        self.asy_lat = None
-        self.lat_sum = None
-        self.weight_sym = None
-        self.weight_asy = None
-        self.avg_sym = None
-        self.avg_asy = None
-        self.time_range = None
-        self.split_data = None
+def sym(
+        lat : np.ndarray,
+        data: np.ndarray,
+) -> np.ndarray:
+    """
+    This function calculates the symmetric component of the data
+    Parameters
+    ----------
+    lat : np.ndarray
+        The latitude values
+    data : np.ndarray
+        The data values
+    Returns
+    -------
+    np.ndarray
+        The symmetric component of the data
+    """
+    sym_lat = np.cos(np.deg2rad(lat))
+    lat_sum = np.sum(sym_lat)
+    
+    weighted = data * sym_lat[None, :, None]
 
-    def sym_format(self, lat, data):
-        sym_lat = np.cos(np.deg2rad(lat))
-        self.lat_sum = np.sum(sym_lat)
-        
-        weighted = data * sym_lat[None, :, None]
+    avg_sym = np.sum(weighted, axis=1) / lat_sum
 
-        avg_sym = np.sum(weighted, axis=1) / self.lat_sum
+    return avg_sym
 
-        return avg_sym
+def asy(
+        lat : np.ndarray,
+        data: np.ndarray,
+)->np.ndarray:
+    """
+    This function calculates the asymmetric component of the data
+    Parameters
+    ----------
+    lat : np.ndarray
+        The latitude values
+    data : np.ndarray
+        The data values
+    Returns
+    -------
+    np.ndarray
+        The asymmetric component of the data
+    """
+    if lat[0] < 0.0:
+        lat = lat[::-1]
+        data = data[:, ::-1, :]
 
-    def asy_format(self, lat, data):
-        if lat[0] < 0.0:
-            lat = lat[::-1]
-            data = data[:, ::-1, :]
+    lat_sum = np.sum(np.cos(np.deg2rad(lat)))
 
-        asy_lat = np.empty_like(lat)
-        asy_lat[: int(lat.shape[0] / 2)] = np.cos(
-            np.deg2rad(lat[: int(lat.shape[0] / 2)])
-        )
-        asy_lat[int(lat.shape[0] / 2) :] = -np.cos(
-            np.deg2rad(lat[int(lat.shape[0] / 2) :])
-        )
+    asy_lat = np.empty_like(lat)
+    asy_lat[: int(lat.shape[0] / 2)] = np.cos(
+        np.deg2rad(lat[: int(lat.shape[0] / 2)])
+    )
+    asy_lat[int(lat.shape[0] / 2) :] = -np.cos(
+        np.deg2rad(lat[int(lat.shape[0] / 2) :])
+    )
 
-        weighted = data * asy_lat[None, :, None]
+    weighted = data * asy_lat[None, :, None]
 
-        self.avg_asy = np.sum(weighted, axis=1) / self.lat_sum
+    avg_asy = np.sum(weighted, axis=1) / lat_sum
 
-        return self.avg_asy
+    return avg_asy
 
-def background(data, num_of_passes):
-    padded_data = np.empty((data.shape[0], data.shape[1] + 2), dtype=data.dtype)
-    padded_data[:, 0] = data[:, 1]
-    padded_data[:, -1] = data[:, -2]
-    padded_data[:, 1:-1] = data
+def background(
+        data        : np.ndarray,
+        smoothtimes : int,
+) -> np.ndarray:
+    """
+    Applies background smoothing to the input data.
 
-    for k in range(num_of_passes):
-        for j in range(padded_data.shape[0]):
-            for i in range(1, padded_data.shape[1] - 1):
-                padded_data[j, i] = (
-                    padded_data[j, i - 1]
-                    + padded_data[j, i] * 2
-                    + padded_data[j, i + 1]
-                ) / 4
+    Parameters:
+        data (np.ndarray): The input data to be smoothed.
+        smoothtimes (int): The number of times the smoothing operation should be applied.
 
-    return padded_data[:, 1:-1]
+    Returns:
+        np.ndarray: The smoothed data.
 
+    """
+    smoothed = np.copy(data)
+    for _ in range(smoothtimes):
+        padded = np.pad(smoothed, (1, 1), mode="reflect")
+        smoothed = (
+            padded[:, :-2] + padded[:, 1:-1]*2 + padded[:, 2:]
+        ) / 4.0
+    return smoothed
